3f435f868e7d42ae5345a7ef1eca6626127e2439
Kajaruban Surendran <surenkajan@gmail.com>
2017-11-08 20:56:44 -0500

    Creating Project template for DocCore

---
+++ src/UoW.DocCore/UoW.DocCore.Web.WebForms/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UoW.DocCore.Web.WebForms.Startup))]
+namespace UoW.DocCore.Web.WebForms
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
