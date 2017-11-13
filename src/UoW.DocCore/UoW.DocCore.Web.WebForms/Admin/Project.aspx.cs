e2458d686efe865390105f85ccc451a46e0baab7
etom <etom@uwaterloo.ca>
2017-11-12 23:40:23 -0500

    latest changes

---
+++ src/UoW.DocCore/UoW.DocCore.Web.WebForms/Admin/Project.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UoW.DocCore.Web.WebForms.Admin
+{
+    public partial class Project : System.Web.UI.Page
+    {
+        public class FriendProfiles
+        {
+            List<Names> _Friendnames = new List<Names>();
+
+            public List<Names> FriendProfiless()
+            {
+                _Friendnames.Add(new Names()
+                {
+                    FirstName = "Brindha",
+                    LastName = 1,
+
+                });
+                _Friendnames.Add(new Names()
+                {
+                    FirstName = "Enlil",
+                    LastName = 2,
+
+                });
+                _Friendnames.Add(new Names()
+                {
+                    FirstName = "Kajaruban",
+                    LastName = 3,
+
+                });
+                _Friendnames.Add(new Names()
+                {
+                    FirstName = "Shitij",
+                    LastName = 4,
+
+                });
+                _Friendnames.Add(new Names()
+                {
+                    FirstName = "Jaspreet",
+                    LastName = 5,
+
+                });
+                return _Friendnames;
+            }
+
+            public class Names
+            {
+                public string FirstName { get; set; }
+                public int LastName { get; set; }
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                FriendProfiles new1 = new FriendProfiles();
+                ListBox1.DataSource = new1.FriendProfiless();
+                ListBox1.DataTextField = "FirstName";
+                //DropDownList1.DataValueField = "LastName";
+                ListBox1.DataBind();
+
+            }
+        }
+        protected void CreateProject(object sender, EventArgs e)
+        {
+            string message = "";
+            foreach (ListItem item in ListBox1.Items)
+            {
+                if (item.Selected)
+                {
+                    message += item.Text + " " + item.Value + "\\n";
+                }
+            }
+            ClientScript.RegisterClientScriptBlock(this.GetType(), "alert", "alert('" + message + "');", true);
+        }
+        protected void DeleteProject(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ src/UoW.DocCore/UoW.DocCore.Web.WebForms/Admin/ViewAllProject.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UoW.DocCore.Web.WebForms.Admin
+{
+    public partial class ViewAllProject : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // pageload when page is loaded first time
+
+                //FriendProfiles new1 = new FriendProfiles();
+                // DataList1.DataSource = new1.FriendProfiless();
+                //DataList1.DataBind();
+                var products = new List<Product>();
+                products.Add(new Product() { ProductID = 1, Name = "Bike", Price = 150.00 });
+                products.Add(new Product() { ProductID = 2, Name = "Helmet", Price = 19.99 });
+                products.Add(new Product() { ProductID = 3, Name = "Tire", Price = 10.00 });
+
+
+                DataList1.DataSource = products;
+                DataList1.DataBind();
+
+
+
+            }
+
+        }
+
+        protected void outerRep_ItemDataBound(object sender, DataListItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Item || (e.Item.ItemType == ListItemType.AlternatingItem))
+            {
+                var test = new List<Test>();
+                test.Add(new Test() { testName = "Bike1" });
+                test.Add(new Test() { testName = "Bike2" });
+                test.Add(new Test() { testName = "Bike3" });
+
+                //System.Data.DataRowView drv = e.Item.DataItem as System.Data.DataRowView;
+                DataList innerDataList = e.Item.FindControl("DataList2") as DataList;
+                DataList innerDataList1 = e.Item.FindControl("DataList3") as DataList;
+                innerDataList.DataSource = test;
+                innerDataList.DataBind();
+                innerDataList1.DataSource = test;
+                innerDataList1.DataBind();
+            }
+        }
+        public class Product
+        {
+            public int ProductID { get; set; }
+            public string Name { get; set; }
+
+            public double Price { get; set; }
+        }
+        public class Test
+        {
+            public string testName { get; set; }
+
+        }
+
+    }
+}
