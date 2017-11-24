1598f1dcd64d9b0e2a1a82685869e920103568ec
Kajaruban Surendran <surenkajan@gmail.com>
2017-11-23 21:09:13 -0500

    dummy insert values

--- src/UoW.DocCore/DocCoreDB/SchemaObjects/Schemas/doccore/Tables/DocCoreTables_InsertDummyValues.sql
+++ src/UoW.DocCore/DocCoreDB/SchemaObjects/Schemas/doccore/Tables/DocCoreTables_InsertDummyValues.sql
@@ -1,3 +1,20 @@
 USE DocCoreDB
 GO
 
+--INSERT INTO [pictre].[User] (UserName, FirstName, LastName, FullName, EmailAddress, DateOfBirth, Sex)
+--VALUES ('surenkajan', 'Kajaruban', 'Surendran', 'Kajaruban Surendran', 'surenkajan@gmail.com', '19931118 10:34:09 AM', 'Male');
+
+--INSERT INTO [pictre].[User] (UserName, FirstName, LastName, FullName, EmailAddress, DateOfBirth, Sex)
+--VALUES ('enlil', 'Enlil', 'Tom', 'Enlil TOm', 'Enlil@gmail.com', '19931118 10:34:09 AM', 'Female');
+
+--INSERT INTO [pictre].[User] (UserName, FirstName, LastName, FullName, EmailAddress, DateOfBirth, Sex)
+--VALUES ('Sam', 'Sam', 'G', 'Sam Deepak', 'sam@gmail.com', '19931118 10:34:09 AM', 'Male');
+
+--INSERT INTO [pictre].[User] (UserName, FirstName, LastName, FullName, EmailAddress, DateOfBirth, Sex)
+--VALUES ('Brindha', 'Brindha', 'G', 'Brindha G', 'brindha@gmail.com', '19931118 10:34:09 AM', 'Female');
+
+--INSERT INTO [pictre].[User] (UserName, FirstName, LastName, FullName, EmailAddress, DateOfBirth, Sex)
+--VALUES ('shitij', 'Shitij', 'V', 'Shitij V', 'Shitij@gmail.com', '19931118 10:34:09 AM', 'Male');
+
+--INSERT INTO [pictre].[User] (UserName, FirstName, LastName, FullName, EmailAddress, DateOfBirth, Sex)
+--VALUES ('jaspreet', 'Jaspreet', 'S', 'Jaspreet S', 'jaspreet@gmail.com', '19931118 10:34:09 AM', 'Male');
