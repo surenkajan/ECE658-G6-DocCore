ae42deaa0a3984ebc96a6465a138119e26d8ded1
Kajaruban Surendran <surenkajan@gmail.com>
2017-11-27 00:34:11 -0500

    GetDocumentByDocIDWithFileContent is added to get the File Content

---
+++ src/UoW.DocCore/DocCoreDB/SchemaObjects/Schemas/doccore/Programmability/StoredProcedures/CoreGetDocumentByDocIdWithFileContent.proc.sql
@@ -0,0 +1,22 @@
+﻿USE [DocCoreDB]
+GO
+
+IF EXISTS ( SELECT * 
+            FROM   sysobjects 
+            WHERE  id = object_id(N'[doccore].[GetDocumentByDocIDWithFileContent]') 
+                   and OBJECTPROPERTY(id, N'IsProcedure') = 1 )
+BEGIN
+    DROP PROCEDURE [doccore].[GetDocumentByDocIDWithFileContent]
+END
+
+GO
+
+CREATE PROCEDURE [doccore].[GetDocumentByDocIDWithFileContent] 
+	@DocID int, 
+	@UserTablePreFix varchar(10)
+AS
+SELECT [DocID],[Id],[FileName],[FileType],[FileSummary],[FileData],[UploadedBy],[UploadedTime],[FileSizeInKB],[IsCheckedIn],[Modified],[ModifiedBy] 
+FROM [doccore].[Documents] WHERE DocID = @DocID
+GO
+
+--EXEC [doccore].[CoreGetUserByEmailIDtemp] @EmailAddress= 'surenkajan@gmail.com'
